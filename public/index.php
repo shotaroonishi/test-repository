a235032a13174b94185443056c5f71248cc3b3cb
shotaroonishi <o_onishi@kani-design.com>
2021-11-12 13:27:19 +0900

    三回目のコミット

--- public/index.php
+++ public/index.php
@@ -26,36 +26,7 @@
             include($_SERVER['DOCUMENT_ROOT']."/inc/header/header-navi.php");
           ?>
         </header>
-        <main role="main">
-          <div class="page-backgroundmove-container">
-            <div class="page-backgroundmove-items">
-              <figure class="page-backgroundmove-fig"><img src="assets/images/common/sample-02.jpg"/></figure>
-              <figure class="page-backgroundmove-fig2"><img src="assets/images/common/sample-02.jpg"/></figure>
-            </div>
-          </div>
-          <p class="mheight">content これは変数テスト</p>
-          <p>content group component : これは変数テスト !!</p><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-          <div class="page-scrolltrigger-container">
-            <div class="page-scrolltrigger-items">
-              <figure class="page-scrolltrigger-fig page-scrolltrigger-target-a"><img src="assets/images/common/sample-02.jpg"/></figure>
-              <figure class="page-scrolltrigger-fig"><img src="assets/images/common/sample-02.jpg"/></figure>
-            </div>
-            <div class="page-scrolltrigger-items">
-              <figure class="page-scrolltrigger-fig"><img src="assets/images/common/sample-02.jpg"/></figure>
-              <figure class="page-scrolltrigger-fig page-scrolltrigger-target-b"><img src="assets/images/common/sample-02.jpg"/></figure>
-            </div>
-          </div>
-          <div class="page-scrolltrigger-container">
-            <div class="page-scrolltrigger-items">
-              <figure class="page-scrolltrigger-fig page-scrolltrigger-target-a"><img src="assets/images/common/sample-02.jpg"/></figure>
-              <figure class="page-scrolltrigger-fig"><img src="assets/images/common/sample-02.jpg"/></figure>
-            </div>
-            <div class="page-scrolltrigger-items">
-              <figure class="page-scrolltrigger-fig"><img src="assets/images/common/sample-02.jpg"/></figure>
-              <figure class="page-scrolltrigger-fig page-scrolltrigger-target-b"><img src="assets/images/common/sample-02.jpg"/></figure>
-            </div>
-          </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        </main>
+        <main role="main"></main>
         <footer class="footer" role="footer"></footer>
         <div class="page-cursor-container"><i class="page-cursor"></i></div>
       </div>
